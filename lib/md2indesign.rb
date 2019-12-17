7b84868a30c7c053e3679301f52cf73504c0c069
Jxck <block.rxckin.beats@gmail.com>
2019-12-17 19:53:58 +0900

    [encode] refuctoring

--- lib/md2indesign.rb
+++ lib/md2indesign.rb
@@ -16,19 +16,24 @@ module MD2Indesign
   module_function
 
   def encode(path, option)
+    format    = option[:format]
+    highlight = option[:highlight]
+
     ext  = File.extname(path)
     name = File.basename(path, ext)
     dir  = File.dirname(path)
     body = File.read(path)
 
-    formatter = MD2Indesign::Format::formatter(option[:format]).new(highlight: option[:highlight])
+    formatter = MD2Indesign::Format::formatter(format).new(highlight: highlight)
     traverser = MD2Indesign::Markdown::Traverser.new(formatter, dir)
     ast       = MD2Indesign::Markdown::AST.new(body).ast
+
     body      = traverser.start(ast)
-    entry     = OpenStruct.new({title: path, highlight: option[:highlight], body: body})
-    template  = ERB.new(File.read("./template/#{option[:format]}.erb"))
+    template  = ERB.new(File.read("./template/#{format}.erb"))
+
+    entry     = OpenStruct.new({title: path, highlight: highlight, body: body})
     encoded   = template.result(entry.instance_eval{binding}).strip
 
-    File.write("#{dir}/#{name}.#{option[:highlight]}.#{option[:format]}", encoded)
+    File.write("#{dir}/#{name}.#{highlight}.#{format}", encoded)
   end
 end
